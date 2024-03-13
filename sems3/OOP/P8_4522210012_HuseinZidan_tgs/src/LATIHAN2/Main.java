de45dcf37ffbe42f8d7abac2a90c5c0a884c9d10
husein zidan <zidan.idros14@gmail.com>
2024-03-13 12:44:05 +0700

    Main.java

---
+++ sems3/OOP/P8_4522210012_HuseinZidan_tgs/src/LATIHAN2/Main.java
@@ -0,0 +1,11 @@
+package LATIHAN2;
+
+public class Main {public String ingat(){
+    return "Jangan lupa beli Oreo, ya!";
+}
+    public static void main(String[] args) {
+        new Hello().ngomongDong();
+        System.out.println(new Main().ingat());
+    }
+}
+
