c03126cd67bafd2b72ae9744f9b9a1ed03a6af15
husein zidan <zidan.idros14@gmail.com>
2024-03-13 12:32:11 +0700

    update Button.java

---
+++ sems3/OOP/P8_4522210012_HuseinZidan_tgs/src/LATIHAN1/Button.java
@@ -0,0 +1,8 @@
+package LATIHAN1;
+
+public class Button {
+    private Clickable action;
+    void setClickAction(Clickable action){this.action = action;
+    }   void doClick(){ action.onClick();}
+}
+
