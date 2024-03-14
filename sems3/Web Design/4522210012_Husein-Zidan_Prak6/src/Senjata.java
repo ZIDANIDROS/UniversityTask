2a74f5d9e36f969af6e58744b5fad6e1e6f5fc93
husein zidan <zidan.idros14@gmail.com>
2024-03-14 23:06:08 +0700

    Senjata

---
+++ sems3/Web Design/4522210012_Husein-Zidan_Prak6/src/Senjata.java
@@ -0,0 +1,13 @@
+public class Senjata {
+    private String nama;
+    private int kekuatan;
+
+    public Senjata(String nama, int kekuatan) {
+        this.nama = nama;
+        this.kekuatan = kekuatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
