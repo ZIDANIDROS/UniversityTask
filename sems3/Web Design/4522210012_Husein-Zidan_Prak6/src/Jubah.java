4db7b8165202f14c8ec29d1320fc1838fbd6d462
husein zidan <zidan.idros14@gmail.com>
2024-03-14 23:06:27 +0700

    jubah

---
+++ sems3/Web Design/4522210012_Husein-Zidan_Prak6/src/Jubah.java
@@ -0,0 +1,15 @@
+public class Jubah {
+    private String nama;
+    private int pertahanan;
+    private int berat;
+
+    public Jubah(String nama, int pertahanan, int berat) {
+        this.nama = nama;
+        this.pertahanan = pertahanan;
+        this.berat = berat;
+    }
+
+    public String getjubah(){
+        return nama;
+    }
+}
