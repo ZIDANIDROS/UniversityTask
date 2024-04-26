b99aa396dcd712745c9d9c43b8ea4bca45c1bb44
husein zidan <zidan.idros14@gmail.com>
2024-04-26 23:20:45 +0700

    pandas

--- sems4/Kecerdasan Buatan/kb3.py
+++ sems4/Kecerdasan Buatan/kb3.py
@@ -7,3 +7,6 @@ import seaborn as sns
 import matplotlib as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+df = pd.read_csv('/content/drive/MyDrive/placement.csv')
+print(df)
