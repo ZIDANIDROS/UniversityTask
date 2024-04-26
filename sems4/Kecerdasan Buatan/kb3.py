6e8a77de2411e6cbecb2e05c6b3d8f9ce37f984b
husein zidan <zidan.idros14@gmail.com>
2024-04-26 23:20:22 +0700

    import

--- sems4/Kecerdasan Buatan/kb3.py
+++ sems4/Kecerdasan Buatan/kb3.py
@@ -1,3 +1,9 @@
 from google.colab import drive
 drive.mount('/content/drive')
 
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
