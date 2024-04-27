409476c2202a0518eae1608f7b0a7b0610f03397
husein zidan <zidan.idros14@gmail.com>
2024-04-28 02:20:48 +0700

    2

--- sems4/Kecerdasan Buatan/kb3.py
+++ sems4/Kecerdasan Buatan/kb3.py
@@ -9,12 +9,8 @@ from sklearn.model_selection import train_test_split
 
 #-------------------------------------
 
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+df = pd.read_csv('placement.csv')
+df.head()
 
 #-------------------------------------
 
