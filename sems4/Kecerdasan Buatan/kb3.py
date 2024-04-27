0b7d95c787cb1d1bd76b4841310ffec62ffc5c29
husein zidan <zidan.idros14@gmail.com>
2024-04-28 02:24:27 +0700

    sklearn

--- sems4/Kecerdasan Buatan/kb3.py
+++ sems4/Kecerdasan Buatan/kb3.py
@@ -62,9 +62,7 @@ X_train, X_test, y_train, y_test= train_test_split(X, y , test_size=0.3, random_
 
 #-------------------------------------
 
-X = df.drop('status', axis=1)
-y = df['status']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
+from sklearn.preprocessing import StandardScaler
 
 #-------------------------------------
 
