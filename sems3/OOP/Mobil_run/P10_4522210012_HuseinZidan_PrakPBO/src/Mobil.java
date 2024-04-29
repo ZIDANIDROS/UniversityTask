a9087cab94526b1d23130c99d2e95d45cc533b17
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-04-29 20:52:39 +0700

    Update Mobil.java

--- sems3/OOP/Mobil_run/P10_4522210012_HuseinZidan_PrakPBO/src/Mobil.java
+++ sems3/OOP/Mobil_run/P10_4522210012_HuseinZidan_PrakPBO/src/Mobil.java
@@ -1,8 +1,5 @@
 class Mobil {
-    private String nama;
-    private Mesin mesin;
-    private Ban ban;
-    private HeadUnit headUnit;
+   
 
     public Mobil(String nama, Mesin mesin, Ban ban, HeadUnit headUnit) {
         this.nama = nama;
