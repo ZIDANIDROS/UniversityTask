05c4b88e7ca13808d8af95997dd91525a443019d
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-06-01 22:15:26 +0700

    Create cluster.py

---
+++ sems4/Kecerdasan Buatan/cluster.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score, confusion_matrix
+import warnings
+from scipy import stats
+from sklearn.preprocessing import StandardScaler
