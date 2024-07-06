7b29468f439a6255a8cae900be42047824a6aa78
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-07-06 17:40:45 +0700

    Update scrapingWeb.py

--- sems4/Kecerdasan Buatan/scrapingWeb.py
+++ sems4/Kecerdasan Buatan/scrapingWeb.py
@@ -7,3 +7,6 @@ from google.colab import files
 
 from bing_image_downloader import downloader
 downloader.download('gelas putih', limit= 135 , output_dir='dataset_GelasPutih', adult_filter_off=True)
+
+# Lokasi folder yang ingin di-zip
+folder_path = '/content/dataset_GelasPutih/gelas putih'
