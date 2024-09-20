e3c430244d7fcac74bd00a7010cf52f933dcc329
husein zidan <zidan.idros14@gmail.com>
2024-09-20 07:39:50 +0700

    koneksi

---
+++ sems5/PBW/thewedding_2/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+// File: koneksi.php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "buku_tamu_db";
+
+// Membuat koneksi ke MySQL
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
