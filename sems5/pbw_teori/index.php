8ea709dccffd071bbf6064e9b5161fed419037d1
husein zidan <zidan.idros14@gmail.com>
2024-09-26 20:55:14 +0700

    index

---
+++ sems5/pbw_teori/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpus Kerren</title>
+</head>
+
+<body style="background-color: black;color:azure">
+    <?php include 'src/view/layout/navbar.html' ?>
+    <a href="login.php">Login</a>
+    <a href="logout.php">Logout</a>
+</body>
+
+</html>
