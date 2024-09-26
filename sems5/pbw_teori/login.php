9e238e30b5e715cae1508b88057bd264fe9d6fa2
husein zidan <zidan.idros14@gmail.com>
2024-09-26 20:55:23 +0700

    login

---
+++ sems5/pbw_teori/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpus Kerren</title>
+</head>
+
+<body style="background-color: black;color:azure">
+    <?php include 'src/view/layout/navbar.html' ?>
+    <h1>LOGIN</h1>
+    <form action="" method="post"></form>
+</body>
+
+</html>
