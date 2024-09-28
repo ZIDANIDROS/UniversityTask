e0768223cea2282d2663c79876eb600fa6f24217
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-09-28 13:03:34 +0700

    Update tugas2.py

--- sems5/Machine Learning/tugas2.py
+++ sems5/Machine Learning/tugas2.py
@@ -37,3 +37,35 @@ if len(null_columns) > 0:
         print(f"- {column}")
 else:
     print("Tidak ada kolom yang memiliki nilai null.")
+
+
+# Pembersihan Data
+# Menghapus Data yang Kotor
+
+# 2. Outliers -------------------------------------------------------
+#     2.1 cek outliers
+
+column = 'price'  # Kolom yang ingin dicek outliers
+
+# Fungsi untuk mendeteksi outliers menggunakan metode IQR
+def detect_outliers(column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Identifikasi outliers
+    outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+    return outliers
+
+
+# Mendeteksi outliers pada kolom 'price'
+outliers_price = detect_outliers('price')
+print("Outliers pada 'price':")
+outliers_price.head()
+
+# Mendeteksi outliers pada kolom 'enginesize'
+outliers_enginesize = detect_outliers('enginesize')
+print("\nOutliers pada 'enginesize':")
+outliers_enginesize.head()
