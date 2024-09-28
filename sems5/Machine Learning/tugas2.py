fa29acbd482702c96a60ea1b94551bea6eebeb27
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-09-28 13:02:24 +0700

    Update tugas2.py

--- sems5/Machine Learning/tugas2.py
+++ sems5/Machine Learning/tugas2.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# Data Tabel
+file_path = 'CarPrice_Assignment.csv'
+df = pd.read_csv(file_path)
+df
