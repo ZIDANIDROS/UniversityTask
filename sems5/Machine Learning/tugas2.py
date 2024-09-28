3b208752cd54ed3d3fae62f3e01a6c8ace2850c4
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-09-28 13:02:52 +0700

    Update tugas2.py

--- sems5/Machine Learning/tugas2.py
+++ sems5/Machine Learning/tugas2.py
@@ -11,3 +11,15 @@ df
 # melihat isi tipedata tiap atribut
 data_types = df.dtypes
 data_types
+
+# Heatmap
+int_float_columns = df.select_dtypes(include=['int64', 'float64'])
+
+correlation_matrix = int_float_columns.corr()
+
+plt.figure(figsize=(10, 6))
+
+sns.heatmap(correlation_matrix, annot=True, fmt='.2f', cmap='coolwarm', square=True, cbar=True)
+
+plt.title('Heatmap Korelasi Antar Atribut Bertipe int dan floatn/')
+plt.show()
