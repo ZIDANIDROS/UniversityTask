e7b7081b30f38d023470129330a0a36e23b16844
HUSEIN ZIDAN <97558377+ZIDANIDROS@users.noreply.github.com>
2024-10-08 22:39:43 +0700

    Update projek.py

--- sems5/SKB/projek.py
+++ sems5/SKB/projek.py
@@ -3,3 +3,6 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 
 data = pd.read_csv('market_basket_2.csv')
+
+# Mengisi nilai NaN dengan 0
+data_filled = data.fillna(0)
