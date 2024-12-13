d746f0ff2236e66177042597fae1e2eba2532283
husein zidan <zidan.idros14@gmail.com>
2024-12-13 08:57:04 +0700

    materi_26

---
+++ sems5/PBW/stickyprak/resources/app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Policies\StorePolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use App\Models\Store;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Gate::policy(Store::class, StorePolicy::class);
+    }
+}
